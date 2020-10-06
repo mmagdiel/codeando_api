9021bece8033fb7ca17302316c48037f75dc93b7
mmagdiel <magdielmarquezhernandez@gmail.com>
2020-10-06 18:38:08 -0500

    Presentacion

---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting Started
+
+In your terminal:
+
+```Bash
+    npm install --global spectacle-cli
+    spectacle -s my-slides.mdx -t custom-theme.js
+```
+
+- Go for content
+  http://localhost:3000/
+
+- Go for presentation
+  http://localhost:3000/?presenterMode=true
---
+++ custom-theme.js
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    primary: "#62c544",
+    secondary: "#1b4b1b",
+    tertiary: "#f1fafb",
+    quaternary: "#800E08",
+  },
+  fonts: {
+    header: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+  },
+  fontSizes: {
+    h1: "72px",
+    h2: "64px",
+  },
+};
